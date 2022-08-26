1daa0703fcf3f0813813b22c424db4176711474d
Tomas <trivero886@gmail.com>
2022-08-26 15:14:41 -0300

    first commit

---
+++ src/Components/FormStore.js
@@ -0,0 +1,15 @@
+const CompForm = () => {
+  return (
+    <div className="container">
+      <form className="block">
+        <input type="text" placeholder="Input nro 1"></input>
+        <input type="date" placeholder="Input nro 2"></input>
+        <input type="text" placeholder="Input nro 3"></input>
+        <input type="text" placeholder="Input nro 4"></input>
+        <input type="text" placeholder="Input nro 5"></input>
+      </form>
+    </div>
+  );
+};
+
+export default CompForm;
